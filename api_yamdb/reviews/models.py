09c577cb51c56fec945c2c703d3c48874477587f
Егор Бойко <ego.boiko2018@yandex.ru>
2023-05-14 22:57:18 +0300

    new develop

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -3,7 +3,6 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
-
 def validate_username(value):
     if not value or value == 'me':
         raise ValidationError(
@@ -24,18 +23,19 @@ class User(AbstractUser):
     username = models.CharField(
         max_length=150,
         unique=True,
-        verbose_name='Имя пользователяяяяяяяяяяяяяяяяяяяяяяяяяя',
+        verbose_name='Имя пользователя',
         validators=[UnicodeUsernameValidator(), validate_username],
     )
     email = models.EmailField(
         unique=True, verbose_name='Адрес электронной почты'
-=======
+    )
+
+
 class Review(models.Model):
     title = models.ForeignKey(on_delete=models.CASCADE, related_name='reviews')
     text = models.TextField(verbose_name='Текст')
     author = models.ForeignKey(
         on_delete=models.CASCADE, related_name='reviews'
-
     )
     score = models.PositiveSmallIntegerField(
         verbose_name='Оценка',
