a1216ce2c61019e68eb0a3e67ef24a7568dc611a
Егор Бойко <ego.boiko2018@yandex.ru>
2023-05-14 23:32:54 +0300

    egor2

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,7 +1,7 @@
-from django.core.validators import MaxValueValidator, MinValueValidator
-
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.core.exceptions import ValidationError
 from django.db import models
-
 
 
 def validate_username(value):
@@ -29,54 +29,27 @@ class User(AbstractUser):
     )
     email = models.EmailField(
         unique=True, verbose_name='Адрес электронной почты'
-=======
-class Review(models.Model):
-    title = models.ForeignKey(on_delete=models.CASCADE, related_name='reviews')
-    text = models.TextField(verbose_name='Текст')
-    author = models.ForeignKey(
-        on_delete=models.CASCADE, related_name='reviews'
-
     )
-    score = models.PositiveSmallIntegerField(
-        verbose_name='Оценка',
-        validators=[
-            MinValueValidator(1, 'Оценка не может быть меньше 1'),
-            MaxValueValidator(10, 'Оценка не может быть выше 10'),
-        ],
+    role = models.CharField(
+        max_length=30, choices=USER_ROLES, default=USER, verbose_name='Роль'
     )
-    pub_date = models.DateTimeField(
-        verbose_name='Дата публикации', auto_now_add=True
+    bio = models.TextField(blank=True, verbose_name='Биография')
+    confirmation_code = models.CharField(
+        verbose_name='Код подтверждения', max_length=36, blank=True, null=True
     )
 
     class Meta:
-        verbose_name = 'Отзыв'
-        verbose_name_plural = 'Отзывы'
-
-        constraints = [
-            models.UniqueConstraint(
-                fields=['author', 'title'], name='unique_author_title'
-            )
-        ]
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ('id',)
 
     def __str__(self):
-        return f'{self.title}, {self.score}, {self.author}'
+        return self.username
 
+    @property
+    def is_admin(self):
+        return self.role == self.ADMIN
 
-class Comment(models.Model):
-    review = models.ForeignKey(
-        Review, on_delete=models.CASCADE, related_name='comments'
-    )
-    text = models.TextField(verbose_name='Текст комментария')
-    author = models.ForeignKey(
-        on_delete=models.CASCADE, related_name='comments'
-    )
-    pub_date = models.DateTimeField(
-        verbose_name='Дата комментария', auto_now_add=True
-    )
-
-    class Meta:
-        verbose_name = 'Комментарий'
-        verbose_name_plural = 'Комментарии'
-
-    def __str__(self):
-        return f'{self.author}, {self.pub_date}: {self.text}'
+    @property
+    def is_moderator(self):
+        return self.role == self.MODERATOR
