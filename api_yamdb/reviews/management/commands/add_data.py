14558a28f1ee4db5120796c10090c9f108b19a8a
Alexman2505 <alexman2505@gmail.com>
2023-05-16 23:06:45 +0300

    реализовано обновление базы данных

---
+++ api_yamdb/reviews/management/commands/add_data.py
@@ -0,0 +1,54 @@
+import csv
+import os
+
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+from reviews.models import (
+    Category,
+    Comment,
+    Genre,
+    GenreTitle,
+    Review,
+    Title,
+    User,
+)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        """Добавляет данные из csv файлов в модели.
+        Для добавления записей в бд введите в терминале команду
+        rm db.sqlite3 && python manage.py migrate && python manage.py add_data
+        """
+        csv_files_path = os.path.join(settings.BASE_DIR, 'static/data')
+        dir_path = os.path.abspath(csv_files_path)
+
+        files_models = [
+            ('users.csv', User),
+            ('category.csv', Category),
+            ('genre.csv', Genre),
+            ('titles.csv', Title),
+            ('review.csv', Review),
+            ('comments.csv', Comment),
+            ('genre_title.csv', GenreTitle),
+        ]
+
+        for file, model in files_models:
+            path = os.path.join(dir_path, file)
+
+            obj_list = []
+            with open(path, encoding='utf-8') as csv_file:
+                for obj_dict in csv.DictReader(csv_file):
+                    if isinstance(model(), Title):
+                        category_id = int(obj_dict['category'])
+                        category = Category.objects.get(id=category_id)
+                        obj_dict['category'] = category
+                    if isinstance(model(), (Review, Comment)):
+                        author_id = int(obj_dict['author'])
+                        author = User.objects.get(id=author_id)
+                        obj_dict['author'] = author
+                    obj_list.append(model(**obj_dict))
+                model.objects.bulk_create(obj_list)
+
+        self.stdout.write(self.style.SUCCESS('Данные успешно добавлены.'))
